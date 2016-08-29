b182ff5d7efc3ad88d8524f22ea027ff1991cef7
Tami <catquarks@gmail.com>
2016-08-29 13:12:14 -0400

    removed unused column from secrets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829143203) do
+ActiveRecord::Schema.define(version: 20160829171038) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160829143203) do
 
   create_table "secrets", force: :cascade do |t|
     t.string   "content"
-    t.string   "subject"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "key_id"
